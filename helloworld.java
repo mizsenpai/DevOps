792d7847a7c39a552bbdb3c8ecf080d3c1fa2b86
mizuchan <64897375+mizsenpai@users.noreply.github.com>
2024-02-02 11:15:24 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ package GitRepo;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("jenkins test again");
+        System.out.println("jenkins test again, hope it works");
     }
 }
