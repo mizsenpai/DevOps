a6fa19b41eb58522549bad7da0f32e442457b715
mizuchan <64897375+mizsenpai@users.noreply.github.com>
2024-02-02 11:51:11 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ package GitRepo;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("world hello");
+        System.out.println("world is hello");
     }
 }
